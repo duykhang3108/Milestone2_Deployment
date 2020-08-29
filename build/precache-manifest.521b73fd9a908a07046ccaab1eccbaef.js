b45535b1e96b77cc8d3d5aea84a0787262fb5ba2
duykhang3108 <s3753740@rmit.edu.vn>
2020-08-29 15:54:55 +0700

    trying to redeploy

--- build/precache-manifest.521b73fd9a908a07046ccaab1eccbaef.js
+++ build/precache-manifest.521b73fd9a908a07046ccaab1eccbaef.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "972e258217aaf7f73cee5dc8643caec6",
+    "revision": "021df517ceeea2624da27b27e50f9e5a",
     "url": "/index.html"
   },
   {
-    "revision": "4f3e8ea92ada14fedc23",
-    "url": "/static/css/2.81a61e56.chunk.css"
+    "revision": "f40620bcf10d33bf53f1",
+    "url": "/static/css/2.fcfafe27.chunk.css"
   },
   {
-    "revision": "1b8975d119e2a77357cb",
-    "url": "/static/css/main.edda8b00.chunk.css"
+    "revision": "1eaf305ea5e0f851cb47",
+    "url": "/static/css/main.6f9d0b20.chunk.css"
   },
   {
-    "revision": "4f3e8ea92ada14fedc23",
-    "url": "/static/js/2.9923fd86.chunk.js"
+    "revision": "f40620bcf10d33bf53f1",
+    "url": "/static/js/2.2ed4c2ca.chunk.js"
   },
   {
     "revision": "d7604c302d6af8b17ab4baffb11faf4b",
-    "url": "/static/js/2.9923fd86.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.2ed4c2ca.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1b8975d119e2a77357cb",
-    "url": "/static/js/main.e18d1bd7.chunk.js"
+    "revision": "1eaf305ea5e0f851cb47",
+    "url": "/static/js/main.2a11f820.chunk.js"
   },
   {
     "revision": "73635f645c271e35ff15",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5206ac569f97f291d5958596af6c16c.js"
+  "/precache-manifest.521b73fd9a908a07046ccaab1eccbaef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
